668852bc2401bcd9bf5dd439d16d74c30933daf7
singhlokeshkumar <lokeshbecool@gmail.com>
2022-03-14 19:22:52 +0000

    first commit

---
+++ src/components/CowinDashboard/index.js
@@ -0,0 +1,12 @@
+// Write your code here
+import {Component} from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+const CowinDashboard = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default CowinDashboard
